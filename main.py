966a958d983534ca90e32e340f0fc5b482802864
Eric Marques <erc_m@hotmail.com>
2020-06-02 00:11:24 -0300

    challenge complete

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import jwt
 
+secret = 'acelera'
+data = {'language': 'Python'}
+
 
 def create_token(data, secret):
-    raise NotImplementedError()
-
+    encoded = jwt.encode(data, secret, algorithm='HS256')    
+    return encoded
 
 def verify_signature(token):
-    raise NotImplementedError()
+    try:
+        return jwt.decode(token, secret, algorithms='HS256')
+    except jwt.DecodeError:
+        return {'error': 2}
+    else:
+        return token
+
+new_token = create_token(data,secret)
+verify_signature(new_token)
