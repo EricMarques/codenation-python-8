8d72df1e021d66e2fc3455478382e07a61c3c5b5
Eric Marques <erc_m@hotmail.com>
2020-06-03 20:47:47 -0300

    correção código

--- main.py
+++ main.py
@@ -5,16 +5,13 @@ data = {'language': 'Python'}
 
 
 def create_token(data, secret):
-    encoded = jwt.encode(data, secret, algorithm='HS256')    
-    return encoded
+    return jwt.encode(data, secret, algorithm='HS256')    
 
 def verify_signature(token):
     try:
         return jwt.decode(token, secret, algorithms='HS256')
     except jwt.DecodeError:
         return {'error': 2}
-    else:
-        return token
 
 new_token = create_token(data,secret)
 verify_signature(new_token)
